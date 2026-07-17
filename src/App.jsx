a91cac0c8afaaa2e30563896714b67939fec4eed
Adrian Palma <adrianpalmasantana@gmail.com>
2026-07-17 14:53:59 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Button} from './components/ui/button.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="ticks"></div>
+      <section id="spacer">
+        <Button variant="default" size="default">Default</Button>
+      </section>
+    </>
+  )
+}
+
+export default App
