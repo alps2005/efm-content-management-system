2633380a8a6f82de77a17b5fba2fe12178977a92
Adrian Palma <adrianpalmasantana@gmail.com>
2026-07-20 11:49:44 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import {Button} from './components/ui/button.jsx'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+// import {Button} from './components/ui/button.jsx'
 import './App.css'
+
+
 
 function App() {
 
   return (
-    <>
-      <div className="ticks"></div>
-      <section id="spacer">
-        <Button variant="default" size="default">Default</Button>
-      </section>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<div>Home</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
