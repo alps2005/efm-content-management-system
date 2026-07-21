fde6160754b21bb90c726226c057a819ddc54644
Adrian Palma <adrianpalmasantana@gmail.com>
2026-07-21 09:40:14 -0500

    [feat] react router used to route login and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-// import {Button} from './components/ui/button.jsx'
-import './App.css'
-
-
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/login/LoginPage"
+import HomePage from "./pages/home/HomePage";
+import "./App.css";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<div>Home</div>} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
